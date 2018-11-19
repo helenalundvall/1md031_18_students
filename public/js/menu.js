24c9c3c1358ebbcf886fd15fb1c81dd0e0c7ed78
helenalundvall <macbook@MacbooksMacBook.lan>
2018-11-19 17:45:54 +0100

    version that is breaking the server

---
+++ public/js/menu.js
@@ -0,0 +1,24 @@
+var food = [
+  {
+  "name": "The Basic burger",
+  "kcal": 800,
+  "allergens": "Gluten, lactose & sesame seeds",
+  "inStock": 5,
+  "image": "https://res.cloudinary.com/coopsverige/image/upload/w_400,h_400/v1490704882/193361.jpg",
+  "checkBox": "basicCheckbox"
+}, {
+  "name": "The Veggie burger",
+  "kcal": 600,
+  "allergens": "Gluten",
+  "inStock": 2,
+  "image": "https://www.ica.se//icase.azureedge.net/imagevaultfiles/id_161415/cf_259/halloumiburgare-med-srirachamajonas-och-soltorkade.jpg",
+  "checkBox": "veggieCheckbox"
+}, {
+  "name": "The Hipster burger",
+  "kcal": 930,
+  "allergens": "Gluten & sesame seeds",
+  "inStock": 4,
+  "image": "https://i.pinimg.com/564x/e9/fd/83/e9fd83f1d9beb90948dea222180d13f1.jpg",
+  "checkBox": "hipsterCheckbox"
+}
+]
---
+++ public/js/vue_script.js
@@ -0,0 +1,78 @@
+var socket = io();
+
+var vm = new Vue ({
+  el: '#myID',
+  data: {
+    burgersArray: food,
+
+    deliveryName: "",
+    deliveryMail: "",
+    deliveryStreet: "",
+    deliveryHouse: 0,
+    selectedPaymentMethod: "",
+    gender: "",
+    checkedBurgers: [],
+
+    orders: {},
+  },
+  created: function () {
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
+  methods: {
+      markDone: function() {
+        document.getElementById("orderID").style.display = "block";
+      },
+        getNext: function () {
+          var lastOrder = Object.keys(this.orders).reduce( function (last, next) {
+            return Math.max(last, next);
+          }, 0);
+          return lastOrder + 1;
+        },
+        addOrder: function (event) {
+          console.log("testerrorinfunctionaddorder");
+          socket.emit("addOrder"), { orderId: this.getNext(),
+            details: { x: event.clientX-10 - event.currentTarget.getBoundingClientRect().left,
+              y: event.clientY-10 - event.currentTarget.getBoundingClientRect().top},
+              orderItems: ["Beans", "Curry"]
+            }
+          }
+        }
+  });
+
+/*
+var vm = new Vue({
+  el: '#dots',
+  data: {
+    orders: {},
+  },
+  created: function () {
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
+  methods: {
+    getNext: function () {
+      var lastOrder = Object.keys(this.orders).reduce( function (last, next) {
+        return Math.max(last, next);
+      }, 0);
+      return lastOrder + 1;
+    },
+    addOrder: function (event) {
+      socket.emit("addOrder", { orderId: this.getNext(),
+        details: { x: event.clientX-10 - event.currentTarget.getBoundingClientRect().left,
+          y: event.clientY-10 - event.currentTarget.getBoundingClientRect().top},
+          orderItems: ["Beans", "Curry"]
+        });
+       }
+     }
+   });*/
